ba93cd0bd8befc068b82ab0ccde18c0933ed9912
Tommaso Figlia <tommasofiglia@gmail.com>
2021-01-29 17:03:46 +0100

    snack 1 - creati gli array

--- Snack 1/index.php
+++ Snack 1/index.php
@@ -1,1 +1,50 @@
-<?php  ?>
+<?php
+
+$matches = [
+  [
+    'casa' => 'Olimpia Milano',
+    'ospite' => 'Cantù',
+    'punticasa' => rand(60, 120),
+    'puntiospite' => rand(60, 120)
+  ],
+  [
+    'casa' => 'Trieste',
+    'ospite' => 'Varese',
+    'punticasa' => rand(60, 120),
+    'puntiospite' => rand(60, 120)
+  ],
+  [
+    'casa' => 'Cremona',
+    'ospite' => 'Virtus Bologna',
+    'punticasa' => rand(60, 120),
+    'puntiospite' => rand(60, 120)
+  ],
+  [
+    'casa' => 'Brescia',
+    'ospite' => 'Universo Treviso',
+    'punticasa' => rand(60, 120),
+    'puntiospite' => rand(60, 120)
+  ],
+  [
+    'casa' => 'Trento',
+    'ospite' => 'Dinamo Sassari',
+    'punticasa' => rand(60, 120),
+    'puntiospite' => rand(60, 120)
+  ],
+  [
+    'casa' => 'Reggiana',
+    'ospite' => 'VL Pesaro',
+    'punticasa' => rand(60, 120),
+    'puntiospite' => rand(60, 120)
+  ],
+  [
+    'casa' => 'Venezia',
+    'ospite' => 'Fortitudo Bologna',
+    'punticasa' => rand(60, 120),
+    'puntiospite' => rand(60, 120)
+  ]
+];
+
+var_dump($matches);
+
+ ?>
